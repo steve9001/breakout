1171537c9a66a10dcc21098e896fe6c37fdb925a
Steve Masterman <steve@vermonster.com>
2011-03-14 22:35:09 -0400

    update worker websocket implementation

--- lib/breakout/web_socket.rb
+++ lib/breakout/web_socket.rb
@@ -1,3 +1,5 @@
+# Adapted from https://github.com/gimite/web-socket-ruby/blob/abbbe0b674cb71fb7658b2792b4ccc5d98e6f0cd/lib/web_socket.rb
+
 # Copyright: Hiroshi Ichikawa <http://gimite.net/en/>
 # Lincense: New BSD Lincense
 # Reference: http://tools.ietf.org/html/draft-hixie-thewebsocketprotocol
@@ -31,12 +33,12 @@ class WebSocket
         end
         @path = $1
         read_header()
-        if !@header["Sec-WebSocket-Key1"] || !@header["Sec-WebSocket-Key2"]
-          raise(WebSocket::Error,
-            "Client speaks old WebSocket protocol, " +
-            "missing header Sec-WebSocket-Key1 and Sec-WebSocket-Key2")
+        if @header["sec-websocket-key1"] && @header["sec-websocket-key2"]
+          @key3 = read(8)
+        else
+          # Old Draft 75 protocol
+          @key3 = nil
         end
-        @key3 = read(8)
         if !@server.accepted_origin?(self.origin)
           raise(WebSocket::Error,
             ("Unaccepted origin: %s (server.accepted_domains = %p)\n\n" +
@@ -89,9 +91,9 @@ class WebSocket
         line = gets().chomp()
         raise(WebSocket::Error, "bad response: #{line}") if !(line =~ /\AHTTP\/1.1 101 /n)
         read_header()
-        if @header["Sec-WebSocket-Origin"] != origin
+        if (@header["sec-websocket-origin"] || "").downcase() != origin.downcase()
           raise(WebSocket::Error,
-            "origin doesn't match: '#{@header["WebSocket-Origin"]}' != '#{origin}'")
+            "origin doesn't match: '#{@header["sec-websocket-origin"]}' != '#{origin}'")
         end
         reply_digest = read(16)
         expected_digest = security_digest(key1, key2, key3)
@@ -114,14 +116,19 @@ class WebSocket
         raise(WebSocket::Error, "handshake has already been done")
       end
       status ||= "101 Web Socket Protocol Handshake"
+      sec_prefix = @key3 ? "Sec-" : ""
       def_header = {
-        "Sec-WebSocket-Origin" => self.origin,
-        "Sec-WebSocket-Location" => self.location,
+        "#{sec_prefix}WebSocket-Origin" => self.origin,
+        "#{sec_prefix}WebSocket-Location" => self.location,
       }
       header = def_header.merge(header)
       header_str = header.map(){ |k, v| "#{k}: #{v}\r\n" }.join("")
-      digest = security_digest(
-        @header["Sec-WebSocket-Key1"], @header["Sec-WebSocket-Key2"], @key3)
+      if @key3
+        digest = security_digest(
+          @header["Sec-WebSocket-Key1"], @header["Sec-WebSocket-Key2"], @key3)
+      else
+        digest = ""
+      end
       # Note that Upgrade and Connection must appear in this order.
       write(
         "HTTP/1.1 #{status}\r\n" +
@@ -166,11 +173,11 @@ class WebSocket
     end
 
     def host
-      return @header["Host"]
+      return @header["host"]
     end
 
     def origin
-      return @header["Origin"]
+      return @header["origin"]
     end
 
     def location
@@ -202,12 +209,13 @@ class WebSocket
           raise(WebSocket::Error, "invalid request: #{line}")
         end
         @header[$1] = $2
+        @header[$1.downcase()] = $2
       end
-      if @header["Upgrade"] != "WebSocket"
-        raise(WebSocket::Error, "invalid Upgrade: " + @header["Upgrade"])
+      if !(@header["upgrade"] =~ /\AWebSocket\z/i)
+        raise(WebSocket::Error, "invalid Upgrade: " + @header["upgrade"])
       end
-      if @header["Connection"] != "Upgrade"
-        raise(WebSocket::Error, "invalid Connection: " + @header["Connection"])
+      if !(@header["connection"] =~ /\AUpgrade\z/i)
+        raise(WebSocket::Error, "invalid Connection: " + @header["connection"])
       end
     end
 
@@ -284,149 +292,4 @@ class WebSocket
       return ssl_socket
     end
 
-end
-
-
-class WebSocketServer
-
-    def initialize(params_or_uri, params = nil)
-      if params
-        uri = params_or_uri.is_a?(String) ? URI.parse(params_or_uri) : params_or_uri
-        params[:port] ||= uri.port
-        params[:accepted_domains] ||= [uri.host]
-      else
-        params = params_or_uri
-      end
-      @port = params[:port] || 80
-      @accepted_domains = params[:accepted_domains]
-      if !@accepted_domains
-        raise(ArgumentError, "params[:accepted_domains] is required")
-      end
-      if params[:host]
-        @tcp_server = TCPServer.open(params[:host], @port)
-      else
-        @tcp_server = TCPServer.open(@port)
-      end
-    end
-
-    attr_reader(:tcp_server, :port, :accepted_domains)
-
-    def run(&block)
-      while true
-        Thread.start(accept()) do |s|
-          begin
-            ws = create_web_socket(s)
-            yield(ws) if ws
-          rescue => ex
-            print_backtrace(ex)
-          ensure
-            begin
-              ws.close_socket() if ws
-            rescue
-            end
-          end
-        end
-      end
-    end
-
-    def accept()
-      return @tcp_server.accept()
-    end
-
-    def accepted_origin?(origin)
-      domain = origin_to_domain(origin)
-      return @accepted_domains.any?(){ |d| File.fnmatch(d, domain) }
-    end
-
-    def origin_to_domain(origin)
-      return origin == "file://" ? "file://" : URI.parse(origin).host
-    end
-
-    def create_web_socket(socket)
-      ch = socket.getc()
-      if ch == ?<
-        # This is Flash socket policy file request, not an actual Web Socket connection.
-        send_flash_socket_policy_file(socket)
-        return nil
-      else
-        socket.ungetc(ch)
-        return WebSocket.new(socket, :server => self)
-      end
-    end
-
-  private
-
-    def print_backtrace(ex)
-      $stderr.printf("%s: %s (%p)\n", ex.backtrace[0], ex.message, ex.class)
-      for s in ex.backtrace[1..-1]
-        $stderr.printf("        %s\n", s)
-      end
-    end
-
-    # Handles Flash socket policy file request sent when web-socket-js is used:
-    # http://github.com/gimite/web-socket-js/tree/master
-    def send_flash_socket_policy_file(socket)
-      socket.puts('<?xml version="1.0"?>')
-      socket.puts('<!DOCTYPE cross-domain-policy SYSTEM ' +
-        '"http://www.macromedia.com/xml/dtds/cross-domain-policy.dtd">')
-      socket.puts('<cross-domain-policy>')
-      for domain in @accepted_domains
-        next if domain == "file://"
-        socket.puts("<allow-access-from domain=\"#{domain}\" to-ports=\"#{@port}\"/>")
-      end
-      socket.puts('</cross-domain-policy>')
-      socket.close()
-    end
-
-end
-
-
-if __FILE__ == $0
-  Thread.abort_on_exception = true
-
-  if ARGV[0] == "server" && ARGV.size == 3
-
-    server = WebSocketServer.new(
-      :accepted_domains => [ARGV[1]],
-      :port => ARGV[2].to_i())
-    puts("Server is running at port %d" % server.port)
-    server.run() do |ws|
-      puts("Connection accepted")
-      puts("Path: #{ws.path}, Origin: #{ws.origin}")
-      if ws.path == "/"
-        ws.handshake()
-        while data = ws.receive()
-          printf("Received: %p\n", data)
-          ws.send(data)
-          printf("Sent: %p\n", data)
-        end
-      else
-        ws.handshake("404 Not Found")
-      end
-      puts("Connection closed")
-    end
-
-  elsif ARGV[0] == "client" && ARGV.size == 2
-
-    client = WebSocket.new(ARGV[1])
-    puts("Connected")
-    Thread.new() do
-      while data = client.receive()
-        printf("Received: %p\n", data)
-      end
-    end
-    $stdin.each_line() do |line|
-      data = line.chomp()
-      client.send(data)
-      printf("Sent: %p\n", data)
-    end
-
-  else
-
-    $stderr.puts("Usage:")
-    $stderr.puts("  ruby web_socket.rb server ACCEPTED_DOMAIN PORT")
-    $stderr.puts("  ruby web_socket.rb client ws://HOST:PORT/")
-    exit(1)
-
-  end
 end
